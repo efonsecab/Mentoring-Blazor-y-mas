199b66376e342cba927864ff3bf0e56a2fefc177
Eduardo Fonseca <eduardo.fonseca@pticostarica.com>
2022-06-16 07:39:14 -0600

    Adding DI basic sample

---
+++ Clase 3/src/MentoringDISln/MyDataAccess/MyCustomConnection.cs
@@ -0,0 +1,19 @@
+﻿namespace MyDataAccess
+{
+    public class MyCustomConnection
+    {
+        private string _connectionString { get; set; }
+
+        public MyCustomConnection(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public string ConnectionString
+        {
+            get { return _connectionString; }
+        }
+
+        //public string ConnectionString => _connectionString;
+    }
+}
